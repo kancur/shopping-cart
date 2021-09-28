a4f83dbb719d0f69a2bb85b4261e0d8fbf074c06
Peter Smid <petosmid@gmail.com>
2021-09-28 18:41:54 +0200

    navlink css fixed

--- src/components/CartIconWithCount..jsx
+++ src/components/CartIconWithCount..jsx
@@ -3,15 +3,15 @@ import { IoCart } from "react-icons/io5";
 import { dataStore } from "./dataStore";
 import { useContext } from "react";
 
-export function CartIconWithCount({ linkTo }) {
+export default  function CartIconWithCount({ linkTo }) {
   const { cart, setCart } = useContext(dataStore);
 
   return (
-    <div className="cart-icon-wrapper">
-      <span className="cart-count">{cart.length}</span>
-      <NavLink to={linkTo}>
+    <NavLink to={linkTo} activeClassName=''>
+      <div className="cart-icon-wrapper">
+        <span className="cart-count">{cart.length}</span>
         <IoCart color="#b1b1b1" />
-      </NavLink>
-    </div>
+      </div>
+    </NavLink>
   );
 }
