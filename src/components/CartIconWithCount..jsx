2e09379bf44cebb89087111e0f23fcf000b60169
Peter Smid <petosmid@gmail.com>
2021-09-29 09:20:42 +0200

    added reducer function to count total products in cart

--- src/components/CartIconWithCount..jsx
+++ src/components/CartIconWithCount..jsx
@@ -4,12 +4,16 @@ import { dataStore } from "./dataStore";
 import { useContext } from "react";
 
 export default  function CartIconWithCount({ linkTo }) {
-  const { cart, setCart } = useContext(dataStore);
+  const { state, dispatch } = useContext(dataStore);
+  
+  const countProductsInCart = () => {
+    return state.reduce((count, product) => count + product.count, 0)
+  }
 
   return (
     <NavLink to={linkTo} activeClassName=''>
       <div className="cart-icon-wrapper">
-        <span className="cart-count">{cart.length}</span>
+        <span className="cart-count">{countProductsInCart()}</span>
         <IoCart color="#b1b1b1" />
       </div>
     </NavLink>
