a2d86c55f67410850c2072de37af8f1389a357da
Peter Smid <petosmid@gmail.com>
2021-12-03 16:30:21 +0100

    add basename for gh-pages compatiblity

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Switch, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import ShoppingCart from "./pages/ShoppingCart";
 import MainTemplate from "./templates/MainTemplate";
 
 const Routes = () => {
   return (
-    //<BrowserRouter basename={process.env.PUBLIC_URL}>
-    <HashRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Route path="/" component={MainTemplate} />
-    </HashRouter>
-    //</BrowserRouter>
+    </BrowserRouter>
   );
 };
 
