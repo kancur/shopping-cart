8614b7ce5939640bfe5e5d8022bd26260758bf87
Peter Smid <petosmid@gmail.com>
2021-12-03 16:29:50 +0100

    change to hashrouter for gh-pages compatibility

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, HashRouter } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import ShoppingCart from "./pages/ShoppingCart";
 import MainTemplate from "./templates/MainTemplate";
 
 const Routes = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    //<BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Route path="/" component={MainTemplate} />
-    </BrowserRouter>
+    </HashRouter>
+    //</BrowserRouter>
   );
 };
 
