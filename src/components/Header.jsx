a4c6ee56331b8b44b9043d0e87ccbfc7d38ebe79
Peter Smid <petosmid@gmail.com>
2021-12-03 16:57:20 +0100

    add link to homepage from logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,20 @@
 import { NavLink } from "react-router-dom";
 import "../styles/header.css";
 import Logo from "../images/zulu-logo.png";
-import '../styles/cartIcon.css';
+import "../styles/cartIcon.css";
 import CartIconWithCount from "./CartIconWithCount.";
 
 export default function Header() {
   return (
     <header>
-      <img className="logo" src={Logo} alt="logo" />
+      <NavLink exact to="/">
+        <img className="logo" src={Logo} alt="logo" />
+      </NavLink>
       <div className="nav">
-        <NavLink exact to="/" >
+        <NavLink exact to="/">
           Home
         </NavLink>
-        <NavLink to="/shop" >
-          Shop
-        </NavLink>
+        <NavLink to="/shop">Shop</NavLink>
         <CartIconWithCount linkTo="/cart" />
       </div>
     </header>
